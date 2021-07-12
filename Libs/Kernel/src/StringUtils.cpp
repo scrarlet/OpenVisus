f48a1291424a4c4de19948a6c1bfc753ff639795
scrarlet <84960690+scrarlet@users.noreply.github.com>
2021-07-12 11:33:33 +0530

    Update StringUtils.cpp

--- Libs/Kernel/src/StringUtils.cpp
+++ Libs/Kernel/src/StringUtils.cpp
@@ -655,7 +655,7 @@ String StringUtils::addEscapeChars(String src)
 
 
 ////////////////////////////////////////////////////////////////////////////////////
-static const char HEX2DEC[256] = 
+static const signed char HEX2DEC[256] = 
 {
   /*       0  1  2  3   4  5  6  7   8  9  A  B   C  D  E  F */
   /* 0 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
